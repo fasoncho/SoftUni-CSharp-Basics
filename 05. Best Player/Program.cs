9c9dbaff1b70ee6f266428ebd8ade860e87fed90
fasoncho <63135017+fasoncho@users.noreply.github.com>
2021-02-19 16:22:02 +0200

    solved

--- 05. Best Player/Program.cs
+++ 05. Best Player/Program.cs
@@ -6,7 +6,33 @@ namespace _05._Best_Player
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string player = Console.ReadLine();
+            string bestPlayer = "";
+            int mostGoals = 0;
+            while (player != "END")
+            {
+                int goalsScored = int.Parse(Console.ReadLine());
+                if (goalsScored > mostGoals)
+                {
+                    bestPlayer = player;
+                    mostGoals = goalsScored;
+                }
+                if (goalsScored >=10)
+                {
+                    break;
+                }
+                player = Console.ReadLine();
+            }
+            Console.WriteLine($"{bestPlayer} is the best player!");
+            if (mostGoals >= 3)
+            {
+                Console.WriteLine($"He has scored {mostGoals} goals and made a hat-trick !!!");
+            }
+            else
+            {
+                Console.WriteLine($"He has scored {mostGoals} goals.");
+            }
+
         }
     }
 }
