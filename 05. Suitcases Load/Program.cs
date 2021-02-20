29b6f0dd7e6a4586d3679c468300408c7ff27a24
fasoncho <63135017+fasoncho@users.noreply.github.com>
2021-02-20 07:54:29 +0200

    solved

---
+++ 05. Suitcases Load/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _05._Suitcases_Load
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double trunkVolume = double.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int suitcaseCounter = 0;
+            while (input != "End")
+            {
+                suitcaseCounter++;
+                double suitcaseVolume = double.Parse(input);
+                if (suitcaseCounter % 3 == 0)
+                {
+                    suitcaseVolume *= 1.1;
+                }
+                trunkVolume -= suitcaseVolume;
+                if (trunkVolume < 0)
+                {
+                    Console.WriteLine("No more space!");
+                    suitcaseCounter -= 1;
+                    break;
+                }
+                input = Console.ReadLine();
+            }
+            if (trunkVolume >= 0)
+            {
+                Console.WriteLine("Congratulations! All suitcases are loaded!");
+            }
+            Console.WriteLine($"Statistic: {suitcaseCounter} suitcases loaded.");
+        }
+    }
+}
---
+++ 06. Tournament of Christmas/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _06._Tournament_of_Christmas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tournamentDays = int.Parse(Console.ReadLine());
+            int days = 0;
+            int daysWon = 0;
+            double moneyRaised = 0;
+            
+            while (tournamentDays > days)
+            {
+                int dayGames = 0;
+                int gamesWon = 0;
+                double dayMoneyRaised = 0;
+                days++;
+                string sport = Console.ReadLine();
+                while (sport != "Finish")
+                {
+                    dayGames++;
+                    string result = Console.ReadLine();
+                    if (result == "win")
+                    {
+                        gamesWon++;
+                        dayMoneyRaised += 20;
+                    }
+                    sport = Console.ReadLine();
+                }
+                if (gamesWon > dayGames - gamesWon)
+                {
+                    daysWon++;
+                    moneyRaised += dayMoneyRaised * 1.1;    
+                }
+                else
+                {
+                    moneyRaised += dayMoneyRaised;
+                }
+                
+            }
+            if (daysWon > days - daysWon)
+            {
+                moneyRaised *= 1.2;
+                Console.WriteLine($"You won the tournament! Total raised money: {moneyRaised:f2}");
+
+            }
+            else
+            {
+                Console.WriteLine($"You lost the tournament! Total raised money: {moneyRaised:f2}");
+            }
+        }
+    }
+}
